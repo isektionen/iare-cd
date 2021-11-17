162dda3192528ce1de98ec9f76a3410d8c89b354
landeholt <johnlan@kth.se>
2021-11-17 16:14:02 +0100

    changed shell

--- app/shell.py
+++ app/shell.py
@@ -9,14 +9,9 @@ def create_shell(service):
     path = PurePath(cwd)
     service_path = PurePath(service["cwd"])
     if path.name == service_path.name:
-        return lambda x: subprocess.Popen(
-            x, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True
-        )
+        return lambda x: subprocess.Popen(x, shell=True)
     else:
         return lambda x: subprocess.Popen(
             f"cd {service['cwd']}; {x}",
-            stdin=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
             shell=True,
         )
